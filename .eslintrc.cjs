972d0cb008a790fec62223b24bd4fa3a06c18c4a
Mikhail Tovt <mikhail.tovt.02@mail.com>
2023-03-06 18:33:00 +0300

    Удалил лишние файлы + настроил борку с форматировщиком кода

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,42 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  env: {
+    node: true
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly'
+  },
+  extends: [
+    'plugin:vue/strongly-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/typescript/recommended',
+    'prettier'
   ],
+  parser: 'vue-eslint-parser',
+  plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
-    ecmaVersion: 'latest'
+    parser: {
+      js: 'espree',
+      ts: '@typescript-eslint/parser',
+      '<template>': 'espree'
+    }
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error',
+    'vue/no-multiple-template-root': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': 'off',
+    'vue/script-setup-uses-vars': 1,
+    'no-return-await': 'warn',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      {
+        registeredComponentsOnly: true
+      }
+    ]
   }
 }
